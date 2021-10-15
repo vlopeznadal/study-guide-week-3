bfbe16003d2d8b43f06792a7608c61bd15cec323
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-14 19:42:27 -0500

    Completed Flask Practice

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -7,20 +7,42 @@ app.secret_key = "blahhhhhhhh"
 def show_homepage():
     return render_template('homepage.html')
 
-###############################
-#                             #
-# 1) Finish the routes below. #
-#                             #
-###############################
-
-
 @app.route('/form')
 def show_form():
-    pass
+    return render_template('form.html')
 
 @app.route('/results')
 def show_results():
-    pass
+
+    cheery = request.args.get("cheery")
+    honest = request.args.get("honest")
+    dreary = request.args.get("dreary")
+
+    # For future reference
+    # Alternative for checkboxs:
+    # if cheery and honest and dreary:
+    #     msg = "Hm, that's a tall order"
+    # elif cheery and honest and not dreary:
+    #     msg = "Here's a cheery and honest message: You're cool!"
+    # elif cheery and dreary and not honest:
+    #     msg = "Cheery AND dreary? Be careful what you wish for. It just might come true."
+    # elif dreary and honest:
+    #     msg = "All good things must come to an end."
+    # elif not dreary and not honest and cheery:
+    #     msg = "Unicorns are real!"
+    # elif not honest and not cheery:
+    #     msg = "All fun activities have been cancelled for the day."
+    # else:
+    #     msg = "You will find what you've been looking for."
+
+    # return render_template('results.html', msg=msg)
+
+    return render_template('results.html', cheery=cheery, honest=honest, dreary=dreary)
+
+@app.route('/save-name')
+def submit_form():
+    session['name'] = request.args.get("name")
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
